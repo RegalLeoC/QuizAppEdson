bc58a135dca21a5ef498b406bf494ce429f3769c
Adalbert047 <aadalberto835@gmail.com>
2024-03-04 21:01:19 -0600

    XML FinPartida

--- app/src/main/java/com/example/proyecto_edson/FinPartida.kt
+++ app/src/main/java/com/example/proyecto_edson/FinPartida.kt
@@ -2,22 +2,58 @@ package com.example.proyecto_edson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class FinPartida : AppCompatActivity() {
+
+
+    private lateinit var imageViewLogo : ImageView
+    private lateinit var textViewResultFinal : TextView
+    private lateinit var textViewQuestion : TextView
+    private lateinit var textViewHist : TextView
+    private lateinit var textViewBonf : TextView
+
+    private var resultValue = 0
+    private var resultQuestion = 0
+    private var resultHis = 0
+    private var resultBonf = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fin_partida)
+
+        imageViewLogo = findViewById(R.id.logoImageResult)
+        textViewResultFinal = findViewById(R.id.textViewResultFin)
+        textViewQuestion = findViewById(R.id.textViewQuest)
+        textViewHist = findViewById(R.id.textViewHist)
+        textViewBonf = findViewById(R.id.textViewBonf)
+
+
+
+
+        validationImg(resultValue)
+
+
     }
     //Adal
 
 
-    private var resultValue = 0
 
 
 
-    fun validationImg(result : Number)
+
+    private fun validationImg(result : Int)
     {
-        
+        if (result in 1..4)
+            imageViewLogo.setImageResource(R.drawable.geography_image)
+        if (result in 7..9)
+            imageViewLogo.setImageResource(R.drawable.mathematics_image)
+        if (result in 12..14)
+            imageViewLogo.setImageResource(R.drawable.img)
+        else
+            imageViewLogo.setImageResource(R.drawable.greek_mythology_image)
     }
 
 
